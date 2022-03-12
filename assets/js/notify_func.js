a92635f15f7c749f5f9efc028cee53e053926137
Hassan Kikabi <hassankikabi@yahoo.com>
2022-03-12 09:43:56 +0400

    Update_012
    Update_012

--- assets/js/notify_func.js
+++ assets/js/notify_func.js
@@ -3,13 +3,13 @@
          $.notify({
              // options
              icon: '',
-             title: "<a href=\"policy.html\" target=\"_blank\"><h4>User Terms Of Use</h4>",
-             message: "<figure><img src=\"assets/img/others/policy_2.png\"></figure><p>Kindly read the terms of service before using Thurayya Islam Media website.</a> "
+             title: "<a href=\"policy.html\"><h4>User Terms Of Use</h4>",
+             message: "<figure><img src=\"assets/img/others/policy_2.png\"></figure><p>Kindly read the terms of service before using Al Hassan Islam Media website.</a> "
          }, {
              // settings
              icon_type: 'image',
              type: 'info',
-             delay: 900,
+             delay: 1000,
              timer: 3000,
              z_index: 9999,
              showProgressbar: false,
